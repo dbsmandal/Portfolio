437e93d552d08ae1bf0f1cf568cfecfc34971072
dbsmandal <dipakmandaljio420@gmail.com>
2022-11-10 21:11:48 +0530

    Folder Structure Created

--- src/Component/Skills.js
+++ src/Component/Skills.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-const App = () => {
+const Skills = () => {
   return (
     <div>
-      personal portfolio
       
     </div>
   )
 }
 
-export default App
+export default Skills
