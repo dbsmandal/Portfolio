43166ff489d8b1803a201818ede92277c102f545
dbsmandal <dipakmandaljio420@gmail.com>
2022-11-10 22:41:38 +0530

    Add Css for Responsive Navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -3,30 +3,31 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            <nav className='flex'>
-                <ul className=''>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <Link to="/projects">Projects</Link>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <Link to="/skills">Skilss</Link>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <Link to="/contact">Conatct</Link>
-                    </li>
-                </ul>
+        <div className='bg-gray-50 dark:bg-gray-700'>
+            <nav className='py-3 px-4 mx-auto max-w-screen-xl md:px-6'>
+                <div className='flex items-center'>
+                    <ul className='flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium'>
+                        <li>
+                            <Link className='text-gray-900 dark:text-white hover:underline' to="/">Home</Link>
+                        </li>
+                    </ul>
+                    <ul className='flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium'>
+                        <li>
+                            <Link className='text-gray-900 dark:text-white hover:underline' to="/projects">Projects</Link>
+                        </li>
+                    </ul>
+                    <ul className='flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium'>
+                        <li>
+                            <Link className='text-gray-900 dark:text-white hover:underline' to="/skills">Skilss</Link>
+                        </li>
+                    </ul>
+                    <ul className='flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium'>
+                        <li>
+                            <Link className='text-gray-900 dark:text-white hover:underline' to="/contact">Conatct</Link>
+                        </li>
+                    </ul>
 
-
+                </div>
             </nav>
 
         </div>
