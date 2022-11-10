573feb19e6d6c98d04f1bb58a440e9aeb6f83345
dbsmandal <dipakmandaljio420@gmail.com>
2022-11-10 21:24:44 +0530

    Routes setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Contact from './Component/Contact';
+import Home from './Component/Home';
+import Navbar from './Component/Navbar';
+import Project from './Component/Project';
+import Skills from './Component/Skills';
+
 
 const App = () => {
   return (
-    <div>
-      personal portfolio
-      
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route to="/" element={<Home/>} />
+          <Route to="/projects" element={<Project/>} />
+          <Route to="/skills" element={<Skills/>} />
+          <Route to="/contact" element={<Contact/>} />
+
+        </Routes>
+      </Router>
+    </>
   )
 }
 
